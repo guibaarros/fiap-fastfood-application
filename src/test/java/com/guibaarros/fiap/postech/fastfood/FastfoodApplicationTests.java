1e767788a1aa487e5a16e1a4c2c352d38b651f8f
Guilherme Barros <guilhermehr00@gmail.com>
2023-10-26 20:43:46 -0300

    first commit

---
+++ src/test/java/com/guibaarros/fiap/postech/fastfood/FastfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guibaarros.fiap.postech.fastfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
